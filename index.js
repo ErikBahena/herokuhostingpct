21ba7c7553a2f8f939f11c99b51db31dc85e4af4
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-02 13:13:10 -0800

    wip

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const server = express();
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 8080;
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 // endpoints
 server.get("/api/users", (req, res) => {
-  res.status(200).json([{ id: 1, name: "Erik" }, {  id: 2, name: "Mike" }, {  id: 3, name: "Tommy" }]);
+  res.status(200).json([
+    { id: 1, name: "Erik" },
+    { id: 2, name: "Mike" },
+    { id: 3, name: "Tommy" },
+  ]);
 });
 
 server.listen(PORT, () => {
